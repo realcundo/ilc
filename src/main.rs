ff9f0869b56130f3f38731bc07ed686d6b16eeab
realcundo <realcundo@users.noreply.github.com>
2019-05-10 22:39:15 +0100

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,6 @@ use std::collections::HashMap;
 use std::io::{self, BufRead};
 use termion::{color, style};
 
-
 fn main() {
     // keeps track of how often each line has occurred
     let mut line_counts = HashMap::new();
@@ -24,7 +23,7 @@ fn main() {
 
         // get the keys and order them by count and then the actual string
         let mut keys: Vec<_> = line_counts.keys().collect();
-        keys.sort_unstable_by_key(|k| (line_counts.get(*k).unwrap(), *k) );
+        keys.sort_unstable_by_key(|k| (line_counts.get(*k).unwrap(), *k));
 
         // TODO don't rewrite if nothing has changed since last time (on the screen)
 
@@ -38,15 +37,23 @@ fn main() {
             print!("{}", termion::clear::All);
         }
 
-        print!("{}", termion::cursor::Goto(1, 1));
-        print!("{}Lines total: {}, Unique lines: {}{}", color::Fg(color::Yellow), lines_total, line_counts.len(), style::Reset);
+        print!(
+            "{}{}Lines total: {}, Unique lines: {}{}",
+            termion::cursor::Goto(1, 1),
+            color::Fg(color::Yellow),
+            lines_total,
+            line_counts.len(),
+            style::Reset
+        );
 
         // iterate over references in reverse to display top first
         for (i, key) in keys.iter().rev().enumerate() {
-            if i+1 >= theight as usize { break; }
+            if i + 1 >= theight as usize {
+                break;
+            }
 
             // render the full output line
-            let out_line = format!("{:width$}: {}", line_counts[*key], key, width=5);
+            let out_line = format!("{:width$}: {}", line_counts[*key], key, width = 5);
 
             let mut out_chars: Vec<char> = out_line.chars().collect();
             out_chars.truncate(twidth as usize);
