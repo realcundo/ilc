75a82aa74b5c024c031e9bd96311322e68d20787
Peter Cunderlik <realcundo@users.noreply.github.com>
2019-07-31 18:57:44 +0100

    Remove slow_linecollector feature

--- src/linecollector.rs
+++ src/linecollector.rs
@@ -35,12 +35,6 @@ impl LineCollector {
         self.line_counts.get(key).cloned()
     }
 
-    #[cfg(feature = "slow_linecollector")]
-    pub fn iter(&self) -> LineCollectorResultIter {
-        LineCollectorResultIter::new(&self.line_counts)
-    }
-
-    #[cfg(not(feature = "slow_linecollector"))]
     pub fn iter(&self) -> impl Iterator<Item = (usize, &String)> {
         self.line_counts
             .iter()
@@ -71,35 +65,6 @@ impl Index<&str> for LineCollector {
 
     fn index(&self, key: &str) -> &Self::Output {
         &self.line_counts[key]
-    }
-}
-
-#[cfg(feature = "slow_linecollector")]
-pub struct LineCollectorResultIter<'a> {
-    sorted_lines: Vec<(usize, &'a String)>, // TODO use str
-}
-
-// TODO implemenmt IntoIterator?!
-#[cfg(feature = "slow_linecollector")]
-impl<'a> LineCollectorResultIter<'a> {
-    fn new(line_counts: &'a HashMap<String, usize>) -> Self {
-        // get all key+value pairs, swap them around and dereference the value (count)
-        // so that it is copied by value. It's convenient to have count first since
-        // we can then simply order by count first and string second.
-        let mut sorted_lines: Vec<_> = line_counts.iter().map(|(s, c)| (*c, s)).collect();
-
-        sorted_lines.sort_unstable();
-        LineCollectorResultIter { sorted_lines }
-    }
-}
-
-#[cfg(feature = "slow_linecollector")]
-impl<'a> Iterator for LineCollectorResultIter<'a> {
-    type Item = (usize, &'a String);
-
-    fn next(&mut self) -> Option<Self::Item> {
-        // return in reverse order so just keep popping from the vector
-        self.sorted_lines.pop()
     }
 }
 
